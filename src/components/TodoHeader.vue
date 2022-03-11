7a63646789d685fcc10cc960f338053682b78a82
bj <byeongjin.kim@wisecommerce.io>
2022-03-11 17:13:09 +0900

    set main

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,25 @@
+<!-- Logo + Date -->
+<script setup>
+import { ref, computed } from 'vue'
+const timestamp = ref('')
+
+function created() {
+  const now = new Date();
+  const month = now.getMonth() + 1;
+  const date = now.getDate();
+  const weekList = new Array("Sun.", "Mon.", "Tue.", "Wed.", "Thu.", "Fri.", "Sat.");
+  const week = weekList[now.getDay()];
+  timestamp.value = `${month}/${date} ${week}`;
+}
+
+const computed_timestamp = computed(
+    get => created()
+)
+</script>
+
+<template>
+  <header class="header">
+    <h1 class="logo">My Todo</h1>
+    <p class="data">{{ timestamp }}</p>
+  </header>
+</template>
---
+++ src/components/TodoTitle.vue
@@ -0,0 +1,19 @@
+<!-- Welcome msg, task cnt -->
+<script setup>
+  import {ref} from 'vue'
+
+  const title_data = ref({
+    message: 'Hello, guest',
+    taskTotal: 5
+  })
+</script>
+
+<template>
+  <div class="title">
+    <p class="title__message">{{ title_data.message }}</p>
+    <p class="title__task">
+      You've got
+      <span class="title__task-total">{{ title_data.taskTotal }}</span> tasks today.
+    </p>
+  </div>
+</template>
